7ddba9b7fed7448f47114faf5d1e9062a737f1ac
Sahil Maheshwari <sahilm1711@gmail.com>
2024-08-06 16:06:53 +0530

    adding createAsCustomer fix

--- src/Concerns/ManagesCustomer.php
+++ src/Concerns/ManagesCustomer.php
@@ -13,53 +13,57 @@ trait ManagesCustomer
      * @return \Laravel\Paddle\Customer
      */
     public function createAsCustomer(array $options = [])
-    {
-        if ($customer = $this->customer) {
-            return $customer;
-        }
-
-        if (! array_key_exists('name', $options) && $name = $this->paddleName()) {
-            $options['name'] = $name;
-        }
-
-        if (! array_key_exists('email', $options) && $email = $this->paddleEmail()) {
-            $options['email'] = $email;
-        }
-
-        if (! isset($options['email'])) {
-            throw new LogicException('Unable to create Paddle customer without an email.');
-        }
-
-        $trialEndsAt = $options['trial_ends_at'] ?? null;
-
-        unset($options['trial_ends_at']);
-
-        // Attempt to find the customer by email address first...
-        $response = Cashier::api('GET', 'customers', [
-            'status' => 'active,archived',
-            'search' => $options['email'],
-        ])['data'][0] ?? null;
-
-        // If we can't find the customer by email, we'll create them on Paddle...
-        if (is_null($response)) {
-            $response = Cashier::api('POST', 'customers', $options)['data'];
-        }
-
-        if (Cashier::$customerModel::where('paddle_id', $response['id'])->exists()) {
-            throw new LogicException("The Paddle customer [{$response['id']}] already exists in the database.");
-        }
-
-        $customer = $this->customer()->make();
-        $customer->paddle_id = $response['id'];
-        $customer->name = $response['name'] ?? '';
-        $customer->email = $response['email'];
-        $customer->trial_ends_at = $trialEndsAt;
-        $customer->save();
-
-        $this->refresh();
-
+   {
+    if ($customer = $this->customer) {
         return $customer;
     }
+
+    if (! array_key_exists('name', $options) && $name = $this->paddleName()) {
+        $options['name'] = $name;
+    }
+
+    if (! array_key_exists('email', $options) && $email = $this->paddleEmail()) {
+        $options['email'] = $email;
+    }
+
+    if (! isset($options['email'])) {
+        throw new LogicException('Unable to create Paddle customer without an email.');
+    }
+
+    $trialEndsAt = $options['trial_ends_at'] ?? null;
+
+    unset($options['trial_ends_at']);
+
+    // Attempt to find the customer by email address first...
+    $response = Cashier::api('GET', 'customers', [
+        'status' => 'active,archived',
+        'search' => $options['email'],
+    ])['data'][0] ?? null;
+
+    // If we can't find the customer by email, we'll create them on Paddle...
+    if (is_null($response)) {
+        $response = Cashier::api('POST', 'customers', $options)['data'];
+    }
+
+    // Check if the Paddle customer already exists in the local database
+    $existingCustomer = Cashier::$customerModel::where('paddle_id', $response['id'])->first();
+    if ($existingCustomer) {
+        return $existingCustomer;
+    }
+
+    // Create and save the new customer in the local database
+    $customer = $this->customer()->make();
+    $customer->paddle_id = $response['id'];
+    $customer->name = $response['name'] ?? '';
+    $customer->email = $response['email'];
+    $customer->trial_ends_at = $trialEndsAt;
+    $customer->save();
+
+    $this->refresh();
+
+    return $customer;
+    }
+
 
     /**
      * Get the customer related to the billable model.
